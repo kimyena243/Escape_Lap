2cf2c77f2b0e8f46856b2e9d11a40d4f6cb8953b
kimyena243 <dpsksla243@naver.com>
2023-05-23 23:54:52 +0900

    적 스폰

--- Escape_Lap/Assets/Scripts/Basic_Fighter.cs
+++ Escape_Lap/Assets/Scripts/Basic_Fighter.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Basic_Fighter : MonoBehaviour
 {
-    Rigidbody2D rd;
-    Transform tr;
-    [SerializeField] private float Speed;
+    protected Rigidbody2D rd;
+    protected Transform tr;
+    [SerializeField] protected float Speed;
     [SerializeField] private float Hp;
     [SerializeField] private float Damage;
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject Bullet;
+    [SerializeField] private GameObject Player;
+    [SerializeField] protected GameObject Bullet;
     
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,7 @@ public class Basic_Fighter : MonoBehaviour
         rd = gameObject.GetComponent<Rigidbody2D>();
         tr = gameObject.GetComponent<Transform>();
         Move();
-        InvokeRepeating("Shoot", 0f,1.0f) ;
+        ShootStart();
         
     }
 
@@ -31,13 +31,20 @@ public class Basic_Fighter : MonoBehaviour
     {
         rd.AddForce(Vector2.down * Speed);
     }
-    virtual public void Shoot() // 기본 공격 
+    virtual public void ShootStart() // 기본 공격 
     {
-        Instantiate(Bullet, tr);
+        StartCoroutine(Shoot());
 
         
     }
-
+    IEnumerator Shoot()
+    {
+        while(true)
+        {
+            Instantiate(Bullet, tr);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
     void Attack()
     {
         Debug.Log("플레이어랑 충돌");
@@ -46,24 +53,32 @@ public class Basic_Fighter : MonoBehaviour
     }
 
     void Delete()
-    { 
-       Destroy(this.gameObject);
-       Debug.Log("머징");
+    {
+        if (Hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Attack();
+            Attack(); // 플레이어hp가져와서 줄이기
         }
 
         if (collision.CompareTag("Delete")) //범위 밖으로 나갔을 때 삭제
         {
+            Destroy(this.gameObject);
+        }
+        if (collision.CompareTag("PlayerBullet")) //플레이어 총알에맞았을때
+        {
+            Hp--; //플레이어 데미지 가져와서 줄이기
             Delete();
+
         }
 
     }
 
-    
-  
+
+
 }
---
+++ Escape_Lap/Assets/Scripts/Fighter_Spawn.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter_Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject[] BasicPattern;
+    [SerializeField] private GameObject[] OtherPattern;
+    private int B_random;
+    private int O_random;
+    private Vector3[] pos;
+    private Quaternion[] rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = new Vector3[4];
+        rot = new Quaternion[4]; 
+
+        pos[0] = new Vector3(-2.0f, 4.2f, 0f); // 왼쪽위
+        rot[0] = Quaternion.Euler(0f, 0f, -135f);
+        pos[1] = new Vector3(2.0f, 4.2f, 0f); //오른쪽 위
+        rot[1] = Quaternion.Euler(0f, 0f, 135f);
+        pos[2] = new Vector3(-2.0f, -4.2f, 0f);//왼쪽 아래
+        rot[2] = Quaternion.Euler(0f, 0f, -45f);
+        pos[3] = new Vector3(2.0f, -4.2f, 0f); // 오른쪽 아래
+        rot[3] = Quaternion.Euler(0f, 0f, 45f);
+
+        StartCoroutine(SpawnBasic());
+        StartCoroutine(SpawnOther());
+    }
+    
+    IEnumerator SpawnBasic()
+    {
+        while(true)
+        {
+            B_random = Random.Range(0, 5);
+            Instantiate(BasicPattern[B_random]);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }   
+    IEnumerator SpawnOther()
+    {
+        while(true)
+        {
+            O_random = Random.Range(0, 3);
+            Debug.Log(O_random);
+
+            if (O_random == 0 || O_random == 1)
+            {
+                //float x = Random.Range(-2.5f, 2.5f);
+                Vector3 pos = new Vector3(Random.Range(-2.5f, 2.5f), -5.5f, 0f);
+                Instantiate(OtherPattern[O_random], pos, Quaternion.identity);
+
+            }
+            if (O_random == 2)
+            {
+                int ran = Random.Range(0, 4);
+
+        
+
+                
+                
+                Instantiate(OtherPattern[O_random], pos[ran], rot[ran]);
+              
+            }
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
